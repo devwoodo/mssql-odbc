81af04863e36f49711ec5d1f5d7c3bfe95073ec4
devwoodo <dev.woodok@gmail.com>
2016-09-07 17:38:06 +0900

    N DBConnector class implementation completed.

---
+++ mssql-odbc/test-header.cpp
@@ -0,0 +1,192 @@
+#include "test-header.h"
+
+void HandleDiagnosticRecord(SQLHANDLE      hHandle,
+	SQLSMALLINT    hType,
+	RETCODE        RetCode)
+{
+	SQLSMALLINT iRec = 0;
+	SQLINTEGER  iError;
+	WCHAR       wszMessage[1000];
+	WCHAR       wszState[SQL_SQLSTATE_SIZE + 1];
+
+	if (RetCode == SQL_INVALID_HANDLE)
+	{
+		fwprintf(stderr, L"Invalid handle!\n");
+		return;
+	}
+
+	while (SQLGetDiagRec(hType,
+		hHandle,
+		++iRec,
+		wszState,
+		&iError,
+		wszMessage,
+		(SQLSMALLINT)(sizeof(wszMessage) / sizeof(WCHAR)),
+		(SQLSMALLINT *)NULL) == SQL_SUCCESS)
+	{
+		// Hide data truncated..
+		if (wcsncmp(wszState, L"01004", 5))
+		{
+			fwprintf(stderr, L"[%5.5s] %s (%d)\n", wszState, wszMessage, iError);
+		}
+	}
+}
+
+std::string convCharT(const std::wstring & wstr)
+{
+	return std::string(wstr.cbegin(), wstr.cend());
+}
+std::wstring convCharT(const std::string & str)
+{
+	return std::wstring(str.cbegin(), str.cend());
+}
+
+DBConnector::DBConnector(const std::string dsnName)
+	: /*DBConnector(),*/	//? 왜 안돼지?
+	_dsnName(dsnName.cbegin(), dsnName.cend())
+{
+	//rev DB connecting process..
+	_init();	//rev
+}
+
+DBConnector::~DBConnector()
+{
+	if (_connectionFlag == true)
+	{
+		close();
+		std::cout << "DBConnector connection closed by destructor." << std::endl;
+	}
+	std::cout << "~DBConnector()" << std::endl;
+	//rev _closeFlag==true 일 때 처리..?
+}
+
+RETCODE DBConnector::connect()
+{
+	std::wstring id, pw;
+	RETCODE errmsg;
+
+	if (_connectionFlag == true)
+	{
+
+		std::cout << "Connection already established." << std::endl;
+		return SQL_SUCCESS;
+	}
+	
+	std::cout << "Database user ID >>";
+	std::getline(std::wcin, _userID);
+	std::cout << "Password >>";
+	std::getline(std::wcin, _pswd);
+
+	if (SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &_hEnv) != SQL_SUCCESS) {
+		std::cout << "Alloc handle error: SQL_HANDLE_ENV" << std::endl;
+		exit(1);
+	}
+	if (SQLSetEnvAttr(_hEnv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0) != SQL_SUCCESS) {
+		std::cout << "Set Attribute error: SQL_HANDLE_ENV" << std::endl;
+		exit(1);
+	}
+
+	if (SQLAllocHandle(SQL_HANDLE_DBC, _hEnv, &_hDbc) != SQL_SUCCESS) {
+		std::cout << "Alloc handle error: SQL_HANDLE_DBC" << std::endl;
+		exit(1);
+	}
+	SQLSetConnectAttr(_hDbc, 5, (SQLPOINTER)SQL_LOGIN_TIMEOUT, 0);
+	
+	//if (RETCODE errmsg = SQLConnect(hDbc, dsnName, SQL_NTS, (SQLWCHAR*)userID, SQL_NTS, (SQLWCHAR*)pswd, SQL_NTS) == SQL_ERROR)			// ok
+	if (errmsg = SQLConnect(_hDbc, const_cast<wchar_t *>(_dsnName.c_str()), SQL_NTS, const_cast<wchar_t *>(_userID.c_str()), SQL_NTS, const_cast<wchar_t*>(_pswd.c_str()), SQL_NTS) == SQL_ERROR)		// ok
+																																										//if (RETCODE errmsg = SQLDriverConnect(hDbc, GetDesktopWindow(), L"", SQL_NTS, NULL, 0, NULL, SQL_DRIVER_COMPLETE) == SQL_ERROR)	// ok
+																																										// connection이 성공 시 SQL_SUCCESS, 혹은 SQL_SUCCESS_WITH_INFO 반환. 
+	{
+		std::cout << "DB connection error." << std::endl;
+		switch (errmsg) {
+		case SQL_ERROR:
+			std::cout << "SQL_ERROR" << std::endl;
+			break;
+		case SQL_SUCCESS_WITH_INFO:
+			std::cout << "SQL_SUCCESS_WITH_INFO" << std::endl;
+			break;
+		case SQL_INVALID_HANDLE:
+			std::cout << "SQL_INVALID_HANDLE" << std::endl;
+			break;
+		default:
+			std::cout << "etc error" << std::endl;
+			break;
+		}
+		HandleDiagnosticRecord(_hDbc, SQL_HANDLE_DBC, errmsg);
+		exit(1);
+	}
+	
+	_connectionFlag = true;
+
+	return errmsg;
+}
+
+void DBConnector::close()
+{
+	try {
+		SQLFreeHandle(SQL_HANDLE_STMT, _hStmt);
+		SQLFreeHandle(SQL_HANDLE_DBC, _hDbc);
+		SQLFreeHandle(SQL_HANDLE_ENV, _hEnv);  //End the connection
+	}
+	catch (...)
+	{
+		throw;
+	}
+	_connectionFlag = false;
+}
+
+RETCODE DBConnector::excute(const std::string & stmt)
+{
+	std::wstring wstr(stmt.cbegin(), stmt.cend());
+
+	SQLAllocHandle(SQL_HANDLE_STMT, _hDbc, &_hStmt);
+	//RETCODE RetCode = SQLExecDirect(hStmt, (SQLWCHAR*)stmt, SQL_NTS);		// error 42000: systax error  (stmt should be given in wchar(L"~") type.)
+	//RETCODE RetCode = SQLExecDirect(hStmt, stmt, SQL_NTS);	// ok	(stmt type changed to wchar(L"~") type.)
+	RETCODE RetCode = SQLExecDirect(_hStmt, const_cast<wchar_t *>(wstr.c_str()), SQL_NTS);	// ok
+																			// 성공 시 SQL_SUCCESS 반환
+
+	switch (RetCode)
+	{
+	case SQL_SUCCESS_WITH_INFO:
+		std::cout << "SQL_SUCCESS_WITH_INFO" << std::endl;
+		HandleDiagnosticRecord(_hStmt, SQL_HANDLE_STMT, RetCode);
+		// fall through
+	case SQL_SUCCESS:
+		std::cout << "SQLExecDirect(..) success!" << std::endl;
+
+		SQLSMALLINT num;
+		std::cout << "SQLNumResultCols: " << SQLNumResultCols(_hStmt, &num) << std::endl;
+		std::cout << "num: " << num << std::endl;
+		break;
+	case SQL_ERROR:
+		std::cout << "SQL_ERROR" << std::endl;
+		HandleDiagnosticRecord(_hStmt, SQL_HANDLE_STMT, RetCode);
+		break;
+	default:
+		fwprintf(stderr, L"Unexpected return code %hd!\n", RetCode);
+	}
+
+	return RetCode;
+}
+
+SQLHENV DBConnector::hEnv() const
+{
+	return _hEnv;
+}
+SQLHDBC DBConnector::hDbc() const
+{
+	return _hDbc;
+}
+SQLHSTMT DBConnector::hStmt() const
+{
+	return _hStmt;
+}
+DBConnector::DBConnector() : _connectionFlag(false)
+{
+	// left blank intentionally
+	_init();
+}
+void DBConnector::_init()
+{
+	_connectionFlag = false;
+}
---
+++ mssql-odbc/test-header.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <Windows.h>
+
+// Required header:
+#include <sql.h>
+#include <sqltypes.h>
+#include <sqlext.h>
+
+void HandleDiagnosticRecord(SQLHANDLE      hHandle,
+	SQLSMALLINT    hType,
+	RETCODE        RetCode);
+
+std::string convCharT(const std::wstring & wstr);
+std::wstring convCharT(const std::string & str);
+
+typedef struct DBConnector
+{
+public:
+	/* member method */
+	DBConnector(const std::string dsnName);
+	~DBConnector();
+
+	RETCODE connect();
+	void close();
+	RETCODE excute(const std::string & stmt);
+	//void getResultSet();	//rev result set 자료형..?
+
+	// accessor
+	SQLHENV hEnv() const;
+	SQLHDBC hDbc() const;
+	SQLHSTMT hStmt() const;
+public:
+	/* member field */
+private:
+	/* member method */
+	DBConnector();
+
+	void _init();
+private:
+	/* member field */
+	SQLHENV _hEnv;
+	SQLHDBC _hDbc;
+	SQLHSTMT _hStmt;
+
+	std::wstring _dsnName;
+	std::wstring _userID;
+	std::wstring _pswd;
+
+	bool _connectionFlag;
+} DBC;
+
+//rev
+// 1.result를 어떤 식으로 얻을지 정해야 함. 필요한 결과는 '검색된 result 수' 정도.
+//	그러나 일반성을 획득하기 위해 result record set 을 얻도록 해야할까..?
