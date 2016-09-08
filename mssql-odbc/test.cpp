a2184ef19d3d411f45af5639bc6f064b1ae131fc
devwoodo <dev.woodok@gmail.com>
2016-09-08 14:45:28 +0900

    N #tmp Source code doesn't need anymore removed.

--- mssql-odbc/test.cpp
+++ mssql-odbc/test.cpp
@@ -6,8 +6,13 @@ int main()
 	
 	dbc.connect();
 	dbc.excute("select * from tb1");
-	dbc.close();
+	
+	SQLSMALLINT resultNum;
+	dbc.getResultNum(resultNum);
 
+	std::cout << "Result number: " << resultNum << std::endl;
+
+	dbc.close();
 	std::cout << "Program ends." << std::endl;
 
 	return 0;
